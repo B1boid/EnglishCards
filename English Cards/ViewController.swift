62d71b6faa81e313678b28f1c8c01dcf6788ce6a
B1boid <aleksandrvorobev@imac-aleksandr.local>
2019-07-02 21:21:05 +0300

    Initial Commit

---
+++ English Cards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  English Cards
+//
+//  Created by Aleksandr Vorobev on 02.07.2019.
+//  Copyright © 2019 Aleksandr Vorobev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ English CardsTests/English_CardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  English_CardsTests.swift
+//  English CardsTests
+//
+//  Created by Aleksandr Vorobev on 02.07.2019.
+//  Copyright © 2019 Aleksandr Vorobev. All rights reserved.
+//
+
+import XCTest
+@testable import English_Cards
+
+class English_CardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
